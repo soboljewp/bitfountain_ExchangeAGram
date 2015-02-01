3eadd7427cef0edad30b034723279391580c7139
Patrick Dawson <patrick.soboljew@gmail.com>
2015-02-01 18:30:01 +0100

    Finished lesson "ExchangeAGram: Bridging Header"

---
+++ ExchangeAGram/Briding-Header.h
@@ -0,0 +1,9 @@
+//
+//  Briding-Header.h
+//  ExchangeAGram
+//
+//  Created by Patrick Dawson on 01.02.15.
+//  Copyright (c) 2015 Patrick Dawson. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
